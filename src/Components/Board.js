ab8472ed746090b6af00a4d50f3367dd1f2b0d0a
amcmullen9291 <amcmullen9291@gmail.com>
2021-08-08 21:56:54 -0400

    css

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,25 +2,17 @@ import React from 'react';
 import Score from './Score';
 
 export default function Board() {
-
-  let image = document.getElementById('mainBoard');
-
-  if(image){
-  image.style.maxWidth = '78%';
-  image.style.maxHeight = 'auto';
-  image.style.border = '10px solid yellow'
-}
   return (
       <>
     <div id="main">
       <img alt="Elements" id="mainBoard" className="table" src={`${process.env.PUBLIC_URL}/elementTable_Background.png`}/>
     <sidenav id="sidenav">
     <Score/>
-    <center><button>Question</button><br/>
-    <button>Question</button><br/>
-    <button>Question</button><br/>
-    <button>Question</button><br/>
-    <button>Question</button><br/>
+    <center><button className="answerButton">Question</button><br/>
+    <button className="answerButton">Question</button><br/>
+    <button className="answerButton">Question</button><br/>
+    <button className="answerButton">Question</button><br/>
+    <button className="answerButton">Question</button><br/>
     </center>
     </sidenav>
     </div>
