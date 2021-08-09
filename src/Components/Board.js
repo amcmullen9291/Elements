39bbf0e3c5c3eb247fc12751424786a162013e31
amcmullen9291 <amcmullen9291@gmail.com>
2021-08-09 03:43:30 -0400

    attched random and right answers to buttons

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -36,12 +36,31 @@ if(answers.length === 1){
     newList.push(answers[0])
   }
 console.log("new list:", newList);
+  let answer = "FE"
+  let answer1 = document.getElementById(`${newList[0]}`);
+  let answer2 = document.getElementById(`${newList[1]}`);
+  let answer3 = document.getElementById(`${newList[2]}`);
+  let answer4 = document.getElementById(`${newList[3]}`);
+  let answer5 = document.getElementById(`${newList[4]}`);
+  let area1 = document.getElementById('area1');
+  area1.append(answer1);
+  area1.append(answer2);
+  area1.append(answer3);
+  area1.append(answer4);
+  area1.append(answer5);
+  let correctAnswer = document.getElementById('rightanswer');
+  correctAnswer.innerHTML = answer
   }
+
+
+
+
 
 
   return (
       <>
     <div id="main">
+      <p>HOWDY</p>
       <img alt="Elements" id="mainBoard" className="table" src={`${process.env.PUBLIC_URL}/elementTable_Background.png`}/>
     <sidenav id="sidenav">
     <div>
@@ -52,11 +71,15 @@ console.log("new list:", newList);
         <hr/>
     </div>
     <center>
-    <div id="button1"><br/></div>
-    <div id="button2"><br/></div>
-    <div id="button3"><br/></div>
-    <div id="button4"><br/></div>
-    <div id="button5"><br/></div>
+    <div id="area1"><br/></div>
+    <div id="area2"><br/></div>
+    <div id="area3"><br/></div>
+    <div id="area4"><br/></div>
+    <div id="area5"><br/></div>
+    <br/>
+    <hr/>
+    <button onClick={nextQuestion}>Next Question</button>
+
     </center>
     </sidenav>
     </div>
@@ -67,7 +90,6 @@ console.log("new list:", newList);
     <button className="answerButton"id="answer5"  onClick={wrongAnswer}>Answer5</button>
     <br/>
     <hr/>
-    <button onClick={nextQuestion}>Next Question</button>
     </>
   )
 }
