86aacf496614163692c7d142055ec1c13dfe762e
amcmullen9291 <amcmullen9291@gmail.com>
2021-08-09 08:54:37 -0400

    added questions

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,12 +1,14 @@
 
 const Questions = ([
-    {question: "The only letter not in the periodic table is the letter.", answer: "J", placebo1: "Q", placebo2: "V", placebo3: "P", placebo4: "S", placebo5: "W", placebo6: "I"},
-    {question: "When burning, lithium gives off a bright this color flame.", answer: "Red", placebo1: "Blue", placebo2: "Green", placebo3: "Yellow", placebo4: "Orange", placebo5: "Grey", placebo6: "Peach" },
-    {question: "White tin will transform into this color tin when the temperature falls below 13.2 degrees C.", answer: "Grey", placebo1: "Golden", placebo2: "Yellow", placebo3: "Rust", placebo4: "Auburn", placebo5: "Turquoise", placebo6: "Blue" },
-    {question: "As a gas, iodine is a vapor that is this color.", answer: "Purple", placebo1: "Smoke", placebo2: "White", placebo3: "Yellow", placebo4: "Red", placebo5: "None", placebo6: "Black"  },
+    {question: "The only letter not in the periodic table is the letter _______.", answer: "J", placebo1: "Q", placebo2: "V", placebo3: "P", placebo4: "S", placebo5: "W"},
+    {question: "When burning, lithium gives off a bright this color flame.", answer: "Red", placebo1: "Blue", placebo2: "Green", placebo3: "Yellow", placebo4: "Orange", placebo5: "Grey"},
+    {question: "White tin will transform into this color tin when the temperature falls below 13.2 degrees C.", answer: "Grey", placebo1: "Golden", placebo2: "Yellow", placebo3: "Rust", placebo4: "Auburn", placebo5: "Turquoise"},
+    {question: "As a gas, iodine is a vapor that is this color.", answer: "Purple", placebo1: "Smoke", placebo2: "White", placebo3: "Yellow", placebo4: "Red", placebo5: "None"},
     {question: "The country Argentina is named after this element. It would be easy to guess if you saw it written in Latin.", answer: "Ag", placebo1: "Ts", placebo2: "Zn", placebo3: "As", placebo4: "He", placebo5: "K"},
     {question: "The rows of the Periodic Table are called this.", answer: "Periods", placebo1: "Areas", placebo2: "Zones", placebo3: "Series", placebo4: "Groups", placebo5: "Majors"}, 
     {question: "The columns of the Periodic Table are called this.", answer: "Groups", placebo1: "Minors", placebo2: "Zones", placebo3: "Ranks", placebo4: "Models", placebo5: "Tracks"},
+    {question: "In the mid-1800s this metal was more expensive than gold.", answer: "Aluminum", placebo1: "Magnesium", placebo2: "Radium", placebo3: "Neon", placebo4: "Silver", placebo5: "Krypton"},
+    {question: "Though generally not found in its free form, it is the most stable isotope of all the elements.", answer: "Tin", placebo1: "Copper", placebo2: "Calcium", placebo3: "Hydrogen", placebo4: "Helium", placebo5: "Titanium"}
 
 ])
 
